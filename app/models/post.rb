1efb19a2a8fecb803fc5a28047673e3f7904f244
sekkey777 <seki7united@gmail.com>
2022-10-13 04:20:28 +0900

    機能追加　登録時の日時にバリデーションを追加
    終了日時は、開始日よりも前の日は選択できない仕様に変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :start, presence: true
+  validate :start_finish_check
   validates :end, presence: true
   validates :priority, presence: true
+
+  def start_finish_check
+    errors.add(:start, "は開始日時以後の日時を選択してください") if self.start > self.end
+  end
 end
